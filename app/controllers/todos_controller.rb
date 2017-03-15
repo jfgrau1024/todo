f9a49f7d49be1812edbb610fa1e43c78cd748a83
Jean Grau <jeangrau@outlook.es>
2017-03-15 23:14:50 +0000

    quitada redundancia

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+    before_action :set_todo, only: [:edit, :update, :show, :destroy]
+    
     def new
         @todo = Todo.new
     end
@@ -15,15 +17,15 @@ class TodosController < ApplicationController
     
     
     def show
-        @todo = Todo.find(params[:id])
+        
     end
     
     def edit
-        @todo = Todo.find(params[:id])
+        
     end
     
     def update
-       @todo = Todo.find(params[:id])
+       
        if @todo.update(todo_params)
            flash[:notice]= "Todo correcto"
            redirect_to todo_path(@todo)
@@ -38,7 +40,7 @@ class TodosController < ApplicationController
     end
     
     def destroy
-        @todo = Todo.find(params[:id])
+        
         @todo.destroy
         flash[:notice] = "Tarea eliminada"
         redirect_to todos_path
@@ -48,6 +50,10 @@ class TodosController < ApplicationController
     
     private
         
+        def set_todo
+        @todo = Todo.find(params[:id])
+        end
+    
         def todo_params
             params.require(:todo).permit(:name, :description)
         end 
